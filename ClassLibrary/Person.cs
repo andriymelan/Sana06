bc64618dcf45722e5dc79b3dc133f44135e7f342
Melanchenko Andriy <ipz232_mad@student.ztu.edu.ua>
2024-01-20 19:39:44 +0200

    added class library, there created class Person. In person added new fields: LastName, FirstName and Birthday type of DateOnly. In this class was added virtual method ShowInfo, which return string with all info from fields. Also added constructor by default and constructor with all parameters in this class.

---
+++ ClassLibrary/Person.cs
@@ -0,0 +1,25 @@
+﻿namespace ClassLibrary
+{
+    public class Person
+    {
+        protected string LastName {  get; set; }
+        protected string FirstName { get; set; }
+        protected DateOnly Birthday { get; set; }
+        public Person()
+        {
+            LastName = "Petrenko";
+            FirstName = "Petro";
+            Birthday = new DateOnly(2000,1,1);
+        }
+        public Person(string lastName, string firstName, DateOnly birthday)
+        {
+            LastName = lastName;
+            FirstName = firstName;
+            Birthday = birthday;
+        }
+        virtual public string ShowInfo()
+        {
+            return $"{LastName} {FirstName} {Birthday}";
+        }
+    }
+}
