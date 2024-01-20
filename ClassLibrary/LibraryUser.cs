af48e16f9642671a78a92253084569f2c9a8f872
Melanchenko Andriy <ipz232_mad@student.ztu.edu.ua>
2024-01-20 20:57:28 +0200

    added class LibraryUser, base class is Person. In class created fields: TicketNumber, IssuanceDate, MonthConstribution. In class was implemented method ShowInfo, which is override from class Person. Also added Constructor by default and constructor with all parameters

--- ClassLibrary/LibraryUser.cs
+++ ClassLibrary/LibraryUser.cs
@@ -1,12 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ClassLibrary
 {
-    public class LibraryUser
+    public class LibraryUser:Person
     {
+        protected int TicketNumber { get; set; }
+        protected DateOnly IssuanceDate {  get; set; }
+        protected float MonthContribution {  get; set; }
+        public LibraryUser()
+        {
+            TicketNumber = 100000;
+            IssuanceDate = new DateOnly(2000,1,1);
+            MonthContribution = 0;
+        }
+        public LibraryUser(string lastName, string firstName, DateOnly birthday, int ticketNumber, DateOnly issuanceDate, float monthContribution)
+            :base(lastName, firstName, birthday)
+        {
+            TicketNumber = ticketNumber;
+            IssuanceDate = issuanceDate;
+            MonthContribution = monthContribution;
+        }
+        public override string ShowInfo()
+        {
+            return $"{base.ShowInfo()} {TicketNumber} {IssuanceDate} {MonthContribution}";
+        }
     }
 }
