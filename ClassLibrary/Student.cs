3578c4b32b2bcac30d661d1ed6d38a3419015446
Melanchenko Andriy <ipz232_mad@student.ztu.edu.ua>
2024-01-20 20:40:53 +0200

    added class Student, base class is Person. In class created fields: Course, Group, Faculty, University. In class was implemented method ShowInfo, which is override from class Person. Also added Constructor by default and constructor with all parameters

---
+++ ClassLibrary/Student.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public  class Student:Person
+    {
+        protected int Course {  get; set; }
+        protected string Group {  get; set; }
+        protected string Faculty {  get; set; }
+        protected string University { get; set; }
+        public Student()
+        {
+            Course = 1;
+            Group = "IPZ-20-1";
+            Faculty = "ICT";
+            University = "ZSTU";
+        }
+        public Student(string lastName, string firstName, DateOnly birthday, int course, string group, string faculty, string university)
+            :base(lastName, firstName, birthday)
+        {
+            Course = course;
+            Group = group;
+            Faculty = faculty;
+            University = university;
+        }
+        public override string ShowInfo()
+        {
+            return $"{base.ShowInfo()} {Course} {Group} {Faculty} {University}";
+        }
+    }
+}
