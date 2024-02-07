93a1a34c268dc495e4e98a36af4c1f127c361eda
Melanchenko Andriy <ipz232_mad@student.ztu.edu.ua>
2024-02-07 17:29:10 +0200

    delete unnecessary files

--- Sana06/Program.cs
+++ Sana06/Program.cs
@@ -1,13 +1,10 @@
 ﻿using ClassLibrary;
 using System.Text.RegularExpressions;
 
-Person[] peoples = new Person[5];
-peoples[0] = new Person("Ivanov", "Ivan", new DateOnly(2005,10,10));
-peoples[1] = new Entrant("Antonenko", "Anton", new DateOnly(2006,06,08),189, 10.2, "ZSTU");
-peoples[2] = new Student();
-peoples[3] = new Teacher("Shevchenko","Taras",new DateOnly(1984,12,25), "rector", "Leadership", "KPI");
-peoples[4] = new LibraryUser("Stepanenko", "Stepan",new DateOnly(1997,04,21),246753,new DateOnly(2023,01,14),103.2);
-for (int i = 0; i < peoples.Length; i++)
-{
-    Console.WriteLine(peoples[i].ShowInfo());
-}
+Person peoples = new Person();
+peoples = new Person("Ivanov", "Ivan", new DateOnly(2005,10,10));
+peoples = new Entrant("Antonenko", "Anton", new DateOnly(2006,06,08),189, 10.2, "ZSTU");
+peoples = new Student();
+peoples = new Teacher("Shevchenko","Taras",new DateOnly(1984,12,25), "rector", "Leadership", "KPI");
+peoples = new LibraryUser("Stepanenko", "Stepan",new DateOnly(1997,04,21),246753,new DateOnly(2023,01,14),103.2);
+Console.WriteLine(peoples.ShowInfo());
